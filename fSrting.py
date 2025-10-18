5712380c431de5c5755a1c24c81a5740957a62ed
Samira <ahmadi.samira6761@gmail.com>
2025-10-18 11:12:57 +0200

    Add first string exercise

---
+++ fSrting.py
@@ -0,0 +1,37 @@
+#Exercise 1
+name = 'samira'
+star = '*' * 15
+print(star + name + star)
+print(f'{name :*^36}')
+
+
+#Exercise 2
+a = 'mohammad javad '
+b = 'azizi'
+print(((a + b) + ' ' ) * 3)
+
+
+#Exercise 3
+myName = 'samira'
+firstLetter = name[0] * 15
+print(firstLetter +'-' + myName + '-' + firstLetter)
+
+#Exercise 4
+number = 12000
+print(f'your number:{number: ,}')
+
+num2 = 34
+print(f'{num2}')
+
+num3 = 4300000
+print(f'{num3:2,}')
+
+#Exercise 5
+string1 = 'samira'
+print(string1[::-1])
+
+string2 = 'pytech'
+print(string2[::-1])
+
+
+
